dfd687a2bb27934b5ed978dfbead75a203e69627
SEBonduel <sebastien050599@hotmail.fr>
2025-11-07 13:46:15 +0100

    welcome commit

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return 'Dashboard';
+})->middleware('auth')->name('dashboard');
